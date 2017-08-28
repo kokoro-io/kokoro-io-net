b4cf0836047a4199dcce91ad2feab06a07f871fe
pgrho <shipwreck@live.jp>
2017-08-28 11:46:03 +0900

    Added ClientTest

---
+++ src/Shipwreck.KokoroIO.Client.Tests/ClientTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Shipwreck.KokoroIO
+{
+    public class ClientTest
+    {
+        [Fact]
+        public void GetProfileAsyncTest()
+        {
+            using (var c = TestHelper.GetClient())
+            {
+                var p = c.GetProfieAsync().GetAwaiter().GetResult();
+
+                Assert.NotNull(p);
+                Assert.Equal(ProfileType.User, p.Type);
+            }
+        }
+
+        [Fact]
+        public void GetRoomsAsyncTest()
+        {
+            using (var c = TestHelper.GetClient())
+            {
+                var rooms = c.GetRoomsAsync().GetAwaiter().GetResult();
+
+                Assert.NotNull(rooms);
+            }
+        }
+
+        [Fact]
+        public void GetPublicRoomsAsyncTest()
+        {
+            using (var c = TestHelper.GetClient())
+            {
+                var rooms = c.GetPublicRoomsAsync().GetAwaiter().GetResult();
+
+                Assert.NotNull(rooms);
+            }
+        }
+
+        [Fact]
+        public void GetPrivateRoomsAsyncTest()
+        {
+            using (var c = TestHelper.GetClient())
+            {
+                var rooms = c.GetPrivateRoomsAsync().GetAwaiter().GetResult();
+
+                Assert.NotNull(rooms);
+            }
+        }
+
+        [Fact]
+        public void GetDirectMessageRoomsAsyncTest()
+        {
+            using (var c = TestHelper.GetClient())
+            {
+                var rooms = c.GetDirectMessageRoomsAsync().GetAwaiter().GetResult();
+
+                Assert.NotNull(rooms);
+            }
+        }
+    }
+}
---
+++ src/Shipwreck.KokoroIO.Client.Tests/TestHelper.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Shipwreck.KokoroIO
+{
+    internal class TestHelper
+    {
+        private static IConfigurationRoot _Configuration;
+
+        public static IConfigurationRoot Configuration
+            => _Configuration ?? (_Configuration = new ConfigurationBuilder().AddUserSecrets<TestHelper>().Build());
+
+        public static Client GetClient()
+            => new Client()
+            {
+                AccessToken = Configuration["UserAccessToken"]
+            };
+    }
+}
