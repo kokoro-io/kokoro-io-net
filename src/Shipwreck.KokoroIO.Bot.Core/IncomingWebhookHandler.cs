7d350bf77ed62ac910f1af94a07b4255282d41ed
pgrho <shipwreck@live.jp>
2017-08-28 16:03:07 +0900

    Added .NET Core incoming webhook

---
+++ src/Shipwreck.KokoroIO.Bot.Core/IncomingWebhookHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Shipwreck.KokoroIO
+{
+    public sealed class IncomingWebhookHandler
+    {
+        private readonly Action<Message> _Callback;
+
+        public IncomingWebhookHandler(Action<Message> callback)
+        {
+            _Callback = callback;
+        }
+
+        public string CallbackSecret { get; set; }
+
+        public async Task HandleAsync(HttpContext httpContext)
+        {
+            var req = httpContext.Request;
+            var valid = CallbackSecret == null
+                        || (req.Headers.TryGetValue("Authorization", out var sv)
+                            && sv.Count == 1
+                            && sv[0] == CallbackSecret);
+
+            var res = httpContext.Response;
+
+            res.ContentType = "text/plain; charset=utf-8";
+            res.StatusCode = valid ? 200 : 400;
+            using (var w = new StreamWriter(res.Body, new UTF8Encoding(false)))
+            {
+                await w.WriteLineAsync(valid ? "OK" : "NG").ConfigureAwait(false);
+                w.Flush();
+            }
+            if (valid)
+            {
+                try
+                {
+                    using (var s = new StreamReader(req.Body))
+                    using (var jr = new JsonTextReader(s))
+                    {
+                        _Callback(new JsonSerializer().Deserialize<Message>(jr));
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
---
+++ src/Shipwreck.KokoroIO.SampleBot/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Shipwreck.KokoroIO.SampleBot
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var routeBuilder = new RouteBuilder(app);
+
+            string prev = null;
+            var handler = new IncomingWebhookHandler(m => prev = m.RawContent)
+            {
+                CallbackSecret = Configuration["CallbackSecret"]
+            };
+
+            routeBuilder.MapGet("", async (c) =>
+            {
+                c.Response.ContentType = "text/plain; charset=utf-8";
+
+                using (var w = new StreamWriter(c.Response.Body, new UTF8Encoding(false)))
+                {
+                    await w.WriteLineAsync("kokoro.io bot example").ConfigureAwait(false);
+                    await w.WriteLineAsync().ConfigureAwait(false);
+
+                    if (prev == null)
+                    {
+                        await w.WriteLineAsync("No request").ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        await w.WriteLineAsync(prev).ConfigureAwait(false);
+                    }
+                    w.Flush();
+                }
+            });
+            routeBuilder.MapPost("incoming", handler.HandleAsync);
+
+            app.UseRouter(routeBuilder.Build());
+        }
+    }
+}
